cd9f25a5ebc8aae125ca0133f2598926abc952c1
snare0625 <86784170+snare0625@users.noreply.github.com>
2022-11-09 11:14:40 +0200

    Added code file

---
+++ domain.py
@@ -0,0 +1,81 @@
+import requests
+from twilio.rest import Client
+
+#GET STOCK DATA
+def stock_data(stock_name, stock_api_key):
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={stock_name}&apikey={stock_api_key}'
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    return data
+
+def curr_week_close(stock_data):
+    curr_week_close = float(stock_data["Weekly Time Series"]["2022-11-08"]["4. close"])
+    return curr_week_close
+
+def prev_week_close(stock_data):
+    prev_week_close = float(stock_data["Weekly Time Series"]["2022-11-04"]["4. close"])
+    return prev_week_close
+
+def stock_weekly_perc_diff(curr_week_close, prev_week_close):
+    positive_diff = abs(curr_week_close - prev_week_close)
+    perc_stock_diff = round(int((positive_diff/curr_week_close) * 100), 2)
+
+    return perc_stock_diff
+
+def get_news_sources(company_name):
+    url = f"https://newsapi.org/v2/everything?q={company_name}&from=2022-10-09&sortBy=publishedAt&apiKey=ab11debedb824edb807bd5a3551186c0"
+    response = requests.get(url)
+    response.raise_for_status()
+    articles = response.json()["articles"]
+    return articles
+
+def company_article_list(articles, company_name, stock_name):
+    news_list = []
+
+    for article in articles:
+        if (company_name in article["title"] or
+            company_name in article["description"] or
+            company_name in article["content"]):
+            source = {}
+            headline = article['description']
+            brief = article['content']
+            source['stock_name'] = stock_name
+            source['headline'] = headline
+            source['brief'] = brief
+            news_list.append(source)
+            if len(news_list) == 3:
+                break
+    return news_list
+
+def send_message(stock_name, article_no, company_article_list):
+
+    account_sid = "AC53c02b716c92cd490d905c2cb1367a7a"
+    auth_token = "5fbec5d6c66796a9f41c65eb627264b8"
+    client = Client(account_sid, auth_token)
+
+    news_source = company_article_list[article_no]
+
+
+    message = client.messages \
+                    .create(
+                         body=f"{stock_name}: percentage_increase\n"
+                              f"Headline: {news_source['headline']}\n"
+                              f"Brief: {news_source['brief']}",
+                         from_='+14782428486',
+                         to='+27825748542'
+                     )
+
+    return message.sid
+
+
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+import domain
+
+
+
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = "RCW9NZCNDK4UAALL"
+NEWS_API_KEY = "ab11debedb824edb807bd5a3551186c0"
+
+
+stock_data = domain.stock_data(STOCK_NAME, STOCK_API_KEY)
+#print(f"Stock data: {stock_data}")
+current_week_close = domain.curr_week_close(stock_data)
+#print(f"current week close: {current_week_close}")
+prev_week_close = domain.prev_week_close(stock_data)
+#print(f"previous week close: {prev_week_close}")
+weekly_perc_diff = domain.stock_weekly_perc_diff(current_week_close, prev_week_close)
+#print(f"Stock weekly difference: {weekly_perc_diff}")
+news_sources = domain.get_news_sources(COMPANY_NAME)
+#print(f"News sources: {news_sources}")
+company_articles = domain.company_article_list(news_sources, COMPANY_NAME, STOCK_NAME)
+#print(f"Company articles: {company_articles}")
+article_no = random.randint(0, 3)
+
+send_stock_details = domain.send_message(STOCK_NAME, article_no, company_articles)
+print(send_stock_details)
+
